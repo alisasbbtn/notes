7e1570afcd0a2129e0029eb3ff0bb7fd64f1b15c
feeltrustobey <artery.aorta@gmail.com>
2019-01-16 21:02:06 +0200

    Feature: Edit note, delete note, minor UI changes

--- script.js
+++ script.js
@@ -5,37 +5,54 @@ var boardController = (function() {
         this.title = title;
         this.body = body;
         this.date = new Date();
-        this.archived = false;
     }
 
-    var data = {
-        items: [],
-        active: [],
-        archived: []
+    Note.prototype.update = function(title, body) {
+        this.title = title;
+        this.body = body;
     }
+
+    var items = [];
 
     return {
         addNote: function(title, body) {
             var note, id;
 
-            if (data.items.length > 0) {
-                id = data.items[data.items.length - 1].id + 1;
+            if (items.length > 0) {
+                id = items[items.length - 1].id + 1;
             } else {
                 id = 0;
             }
 
             note = new Note(id, title, body);
 
-            data.items.push(note);
-            data.active.push(note.id);
+            items.push(note);
 
             return note;
         },
 
-        getCounters: function() {
-            return {
-                active: data.active.length,
-                archived: data.archived.length
+        deleteNote: function(id) {
+            var ids, index;
+
+            ids = items.map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if(index !== -1) {
+                items.splice(index, 1);
+            }
+        },
+
+        getNotesCounter: function() {
+            return items.length
+        },
+
+        getNote: function(id) {
+            for(i = 0; i < items.length; i++) {
+                if(items[i].id == id)
+                    return items[i];
             }
         }
     }
@@ -44,23 +61,34 @@ var boardController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
     var DOMstrings = {
-        inputTitle: '.add__title',
-        inputBody: '.add__body',
+        inputs: {
+            add: {
+                title: '.add__title',
+                body: '.add__body'
+            },
+            edit: {
+                title: '.edit__title',
+                body: '.edit__body',
+            }
+        },
         inputBtn: '.add__btn',
         container: '.container',
-        counterContainer: {
-            active: '.board__notes--active',
-            archived: '.board__notes--archived'
-        },
+        noteTitle: '.note__title',
+        noteBody: '.note__body',
+        counterContainer: '.board__notes',
         counter: '.counter',
-        counterTitle: '.counter--title'
-    }
+        counterTitle: '.counter--title',
+        editBtn: 'note__edit',
+        deleteBtn: 'note__delete',
+        applyEditBtn: 'edit__note__apply',
+        cnacelEditBtn: 'edit__note__cancel'
+    };
 
     return {
         addBoardNote: function(note) {
             var HTML, newHTML;
 
-            HTML = '<div class="note__container" id="note-%id%"><h1 class="note__title">%title%</h1><p class="note__body">%body%</p><div class="note__meta"><span class="note__date">%date%</span><div class="note__actions"><span class="note__edit ion-edit" title="Edit"></span><span class="note__archive ion-archive" title="Archive"></span><span class="note__delete ion-trash-b" title="Delete"></span></div></div></div>';
+            HTML = '<div class="note__container" id="%id%"><div class="edit__container"><input type="text" class="edit__title" placeholder="Title" /><textarea class="edit__body" placeholder="Edit a note..."></textarea></div><div class="note__content">            <h1 class="note__title">%title%</h1><p class="note__body">%body%</p></div><div class="note__meta"><span class="note__date">%date%</span><div class="note__actions"><button type="button" class="note__edit"><span class="ion-edit" title="Edit"></span></button><button type="button" class="note__delete"><span class="ion-trash-b" title="Delete"></span></button><button type="button" class="edit__note__apply"><span class="ion-checkmark" title="Apply"></span></button><button type="button" class="edit__note__cancel"><span class="ion-close" title="Cancel"></span></button</div></div></div>';
 
             newHTML = HTML.replace('%id%', note.id);
             newHTML = newHTML.replace('%title%', note.title);
@@ -70,10 +98,53 @@ var UIController = (function() {
             document.querySelector(DOMstrings.container).insertAdjacentHTML('beforeend', newHTML);
         },
 
-        getInput: function() {
+        editBoardNote: function(note) {
+            var noteElement;
+
+            noteElement = document.getElementById(note.id);
+
+            noteElement.querySelector(DOMstrings.inputs.edit.title).value = note.title;
+            noteElement.querySelector(DOMstrings.inputs.edit.body).value = note.body;
+
+            noteElement.classList.toggle('editing');
+        },
+
+        deleteBoardNote: function(noteID) {
+            var noteElement;
+
+            noteElement = document.getElementById(noteID);
+
+            noteElement.remove();
+        },
+
+        applyEditBoardNote: function(note) {
+            var noteElement;
+
+            noteElement = this.getNoteContainer(note.id);
+
+            noteElement.querySelector(DOMstrings.noteTitle).innerHTML = note.title;
+            noteElement.querySelector(DOMstrings.noteBody).innerHTML = note.body;
+
+            noteElement.classList.toggle('editing');
+            
+        },
+
+        cancelEditBoardNote: function(noteID) {
+            var noteElement;
+
+            noteElement = this.getNoteContainer(noteID);
+
+            noteElement.classList.toggle('editing');
+        },
+
+        getNoteContainer: function(id) {
+            return document.getElementById(id);
+        },
+
+        getInput: function(container, type) {
             return {
-                title: document.querySelector(DOMstrings.inputTitle).value,
-                body: document.querySelector(DOMstrings.inputBody).value
+                title: container.querySelector(DOMstrings.inputs[type].title).value,
+                body: container.querySelector(DOMstrings.inputs[type].body).value
             }
         },
 
@@ -82,22 +153,18 @@ var UIController = (function() {
         },
 
         clearFields: function() {
-            document.querySelector(DOMstrings.inputTitle).value = "";
-            document.querySelector(DOMstrings.inputBody).value = "";
+            document.querySelector(DOMstrings.inputs.add.title).value = "";
+            document.querySelector(DOMstrings.inputs.add.body).value = "";
         },
 
-        updateCounter: function(counter, type) {
+        updateCounter: function(counter) {
             var counterContainer;
 
-            counterContainer = document.querySelector(DOMstrings.counterContainer[type]);
+            counterContainer = document.querySelector(DOMstrings.counterContainer);
             
-            if(counter > 0) {
-                counterContainer.style.visibility = 'visible';
-                counterContainer.querySelector(DOMstrings.counter).textContent = counter;
-                counterContainer.querySelector(DOMstrings.counterTitle).textContent = (counter === 1 ? 'note' : 'notes');
-            } else {
-                counterContainer.style.visibility = 'hidden';
-            }
+            counterContainer.querySelector(DOMstrings.counter).textContent = counter;
+            counterContainer.querySelector(DOMstrings.counterTitle).textContent = (counter === 1 ? 'note' : 'notes');
+
         }
     }
 })();
@@ -106,34 +173,105 @@ var UIController = (function() {
 var controller = (function(boardCtrl, UICtrl) {
     var DOM = UICtrl.getDOMstrings();
 
-    var updateCounters = function() {
-        var data;
+    var ctrlAddNote = function() {
+        var input, note;
 
-        data = boardCtrl.getCounters();
+        // Get values
+        input = UICtrl.getInput(document, 'add');
 
-        UICtrl.updateCounter(data.active, 'active');
-        UICtrl.updateCounter(data.archived, 'archived');
+        if(input.body !== "") {
+            // Create note
+            note = boardCtrl.addNote(input.title, input.body);
+
+            // Clear fields
+            UICtrl.clearFields();
+
+            // Add note on board
+            UICtrl.addBoardNote(note);
+
+            // Update counters
+            updateCounter();
+        }
+    }
+
+    var ctrlEditNote = function(id) {
+        var note;
+
+        note = boardCtrl.getNote(id);
+
+        UIController.editBoardNote(note);
+    }
+
+    var ctrlApplyEditNote = function(id) {
+        var container, input, note;
+
+        container = UICtrl.getNoteContainer(id);
+
+        input = UICtrl.getInput(container, 'edit');
+
+        if(input.body !== "") {
+            note = boardCtrl.getNote(id);
+
+            note.update(input.title, input.body);
+
+            UICtrl.applyEditBoardNote(note);
+        }
+    }
+
+    var ctrlCancelEditNote = function(id) {
+        UICtrl.cancelEditBoardNote(id);
+    }
+
+    var ctrlDeleteNote = function(id) { 
+        
+        if(confirm('Are you sure?')) {
+            // Delete note from the data
+            boardCtrl.deleteNote(id);
+
+            // Deletenote from board
+            UICtrl.deleteBoardNote(id);
+
+            // Update counters
+            updateCounter();
+        }
+
+    }
+ 
+    var updateCounter = function() {
+        var counter;
+
+        counter = boardCtrl.getNotesCounter();
+
+        UICtrl.updateCounter(counter);
     }
 
     var setupEventListeners = function() {
-        document.querySelector(DOM.inputBtn).addEventListener('click', function() {
-            var input, note;
-    
-            // Get values
-            input = UICtrl.getInput();
-    
-            if(input.body !== "") {
-                // Create note
-                note = boardCtrl.addNote(input.title, input.body);
-    
-                // Clear fields
-                UICtrl.clearFields();
-    
-                // Add note on board
-                UICtrl.addBoardNote(note);
-    
-                // Update counters
-                updateCounters();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddNote);
+
+        document.querySelector(DOM.container).addEventListener('click', function(event) {
+            var noteID, actionName;
+
+            noteID = parseInt(event.target.parentNode.parentNode.parentNode.parentNode.id);
+            if(noteID || noteID === 0) {
+
+
+                actionName = event.target.parentNode.className;
+                
+                switch(actionName) {
+                    case DOM.editBtn:
+                        ctrlEditNote(noteID);
+                        break;
+                    case DOM.deleteBtn:
+                        ctrlDeleteNote(noteID);
+                        break;
+                    case DOM.applyEditBtn:
+                        ctrlApplyEditNote(noteID);
+                        break;
+                    case DOM.cnacelEditBtn:
+                        ctrlCancelEditNote(noteID);
+                        break;   
+                }
             }
         });
     }
@@ -142,7 +280,7 @@ var controller = (function(boardCtrl, UICtrl) {
         init: function() {
             console.log('Application has started.');
 
-            updateCounters();
+            updateCounter();
             setupEventListeners();
         }
     }
